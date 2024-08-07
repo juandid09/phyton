2066dcab2a0efe2481e05147d76715a0f8dae1e8
juan oviedo <juandid.09@gmail.com>
2024-08-07 11:30:32 -0500

    clase 4

---
+++ clase4.py
@@ -0,0 +1,60 @@
+name = "carli"
+#las tres comillas simples interpretan el salto de linea
+apellido = '''wilson
+kiara
+pereza'''
+
+print("nombre")
+print(apellido)
+
+alias = 'wilson perrito'
+print(alias[0])
+print(alias[1])
+print(alias[2])
+print(alias[3])
+print(alias[4])
+print(alias[5])
+
+alias = 'wilson perrito'
+print(alias[-8])
+print(alias[-7])
+print(alias[-6])
+print(alias[-5])
+print(alias[-4])
+print(alias[-3])
+print(alias[-2])
+print(alias[-1])
+
+#concatenas sumar dos cadenas 
+
+gato = "pereza miau"
+alias = "gatito blanco"
+
+print(gato)
+#concateno 
+print(gato + alias)
+
+#concateno con espacio
+print(gato +" "+ alias)
+
+#comillas simples es mejor ''
+
+gato = 'milagrito miau'
+alias = 'gatito amarillo'
+
+#repeticion 5 veces gatito 
+
+print(gato * 5)
+
+#conocer el numero de caracteres len 
+animal='loro'
+print(len(animal))
+
+#metodos 
+animal = ' TORtuga '
+print(animal) #normal
+print(animal.lower()) #minusculas 
+print(animal.upper()) #mayuscula 
+print(animal.strip()) #elimina espacios al inicio y al final 
+
+
