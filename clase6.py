bb082a33375b6df09b6d899a8021e9abbfe0dc86
juan oviedo <juandid.09@gmail.com>
2024-08-07 14:19:40 -0500

    clase 6 operadores

---
+++ clase6.py
@@ -0,0 +1,37 @@
+a = 10
+b = 3
+print("suma:", a+b)
+print("resta:", a-b)
+print("multiplicacion:", a*b)
+print("divicion:", a/b)
+
+#modulo 
+
+print("modulo:", a%b)
+
+#parte entrera de la division
+print("parte entera de la division:", a//b)
+
+#potenciasion 
+print("potenciacion:", a**b)
+
+a = a + 2
+print("resultado de a:",a)
+
+a += 2
+print("resultado de a:",a)
+
+operacion = 2-3*4
+operacion2 = (2-3)*4
+print("operacion:",operacion)
+print("operacion 2:",operacion2)
+
+a = 10
+b = 3
+
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+print(a!=b)
