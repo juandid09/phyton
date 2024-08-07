0a4caa00e14ad62b6d8a1dace61cef34a657cfb0
juan oviedo <juandid.09@gmail.com>
2024-08-07 14:57:36 -0500

    ejercicio 7 input

---
+++ clase7.py
@@ -0,0 +1,15 @@
+#ingreso por cosola todo es string
+nombre = input("ingrese su nombre:")
+
+print("Tu nombre es:",nombre)
+
+#casting 
+
+edad =int(input("ingrese su edad:"))
+print("Tu edad es:",edad)
+print(type(edad))
+
+edad2 =float(input("ingrese su edad:"))
+print("Tu edad es:",edad2)
+print(type(edad2))
+
