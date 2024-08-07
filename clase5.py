7cf0b42c1c2cde7014912838dba11ee713e1a2a8
juan oviedo <juandid.09@gmail.com>
2024-08-07 13:53:49 -0500

    ejercicio 5 variables

---
+++ clase5.py
@@ -0,0 +1,25 @@
+x = 10 #entero int 
+print(type(x)) #type me permite saber el tipo de dato 
+
+y = 3.1416 #floante float
+print(type(y))
+
+z = 1.2e6 #exponencial a 6 
+print(z)
+
+z = 1.2e-6 #exponencial a -6 noacion cientifica 
+print(z)
+
+#opereciones matematicas 
+print(x+x)
+print(x+y)
+print(y+y)
+
+#booleam 
+
+is_true = True
+is_false = False
+
+print(type(is_true))
+print(is_true)
+print(is_false)
